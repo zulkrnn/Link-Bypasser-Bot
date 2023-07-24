e2a664f69de2e67eb72ec03ceee1f91a23afcdb5
zulkrnn <116282702+zulkrnn@users.noreply.github.com>
2023-07-24 16:29:10 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,10 @@
-import os
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return """
-<center> 
-    <img src="https://i.giphy.com/media/3o7abAHdYvZdBNnGZq/giphy.webp" style="border-radius: 12px;"/> 
-</center> 
-<style>
-    body { 
-        background: antiquewhite;
-    }
-</style>"""
+def hello_world():
+    return 'GreyMatters'
+
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
